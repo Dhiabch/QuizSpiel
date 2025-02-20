172ecc67575dfcd06e01d89b9055b1fdf2f9d107
SumayaYousofzei <sumayayosuf@gmail.com>
2025-02-20 12:53:39 +0100

    Benutzer Klasse erstellt

---
+++ QuizDhia2/Benutzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSpiel
+{
+    internal class Benutzer
+    {
+
+        private String name;
+        private String passwort;
+        private int id;
+
+        public void Benuter()
+        {
+            
+        }
+     
+
+        public String getBenuter(string benutzer)
+        {
+            return benutzer;
+        }
+
+
+
+
+
+    }
+}
