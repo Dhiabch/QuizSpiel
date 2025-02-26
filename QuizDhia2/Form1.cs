a3622fdedebe3234eee610cb79c7e18983ef02e2
Dhia.Bouchareb <user@Dhia.de>
2025-02-26 10:53:49 +0100

    Login Form Design

--- QuizDhia2/Form1.cs
+++ QuizDhia2/Form1.cs
@@ -41,5 +41,30 @@ namespace QuizDhia2
         {
 
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
