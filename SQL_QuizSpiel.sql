b926eac4735cecd0cb4f87a9d01cb4e2c61cbceb
SumayaYousofzei <sumayayosuf@gmail.com>
2025-02-26 10:57:04 +0100

    SQL erstellt

---
+++ SQL_QuizSpiel.sql
@@ -0,0 +1,20 @@
+﻿/* Datenbank erzeugen, falls bereits existiert, vorher löschen 
+ CREATE DATABASE quizSpiel*/
+
+/*USE 
+quizSpiel;*/
+
+
+/* CREATE TABLE tblBenutzer (
+       Benutzer_ID int, Email varchar(50), Passwort varchar(128)) */
+
+/*CREATE TABLE tblFrage(
+        Frage_ID int, Beschreibung varchar(1000))*/
+
+/*CREATE TABLE tblAntwort(
+        Antwort_ID int, Beschreibung varchar(1000))*/
+
+
+/*Select  * from tblAntwort*/
+
+
