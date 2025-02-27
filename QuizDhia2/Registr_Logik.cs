729881b8d0381013564b129d40cc671b0caa1828
Aiana Dzhekshenkulova <aidzh@gmail.com>
2025-02-27 12:54:39 +0100

    Connection Datenbank Klasse

--- QuizDhia2/Registr_Logik.cs
+++ QuizDhia2/Registr_Logik.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuizDhia2
 {
@@ -11,11 +12,9 @@ namespace QuizDhia2
     {
         Benutzer benutzer = new Benutzer();
 
-        public String benutzerName()
+        private void LoginBenutzerName(TextBox tfBenutzername)
         {
-            String benutzerName = benutzer.getBenutzer();
-            return benutzerName;
+
         }
-        
     }
 }
