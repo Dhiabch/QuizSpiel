c923b1b40e941a773fc5cfe83c84b16c1e9e26c8
Dhia.Bouchareb <user@Dhia.de>
2025-04-02 10:52:13 +0200

    StartForm erstellt

--- QuizDhia2/StartForm.cs
+++ QuizDhia2/StartForm.cs
@@ -16,25 +16,5 @@ namespace QuizDhia2
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
