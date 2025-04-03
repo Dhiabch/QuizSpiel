313c2fe618ab2640a641867b80442daca22935e4
SumayaYousofzei <sumayayosuf@gmail.com>
2025-04-03 12:58:27 +0200

    StartForm Logic erstellt

---
+++ QuizDhia2/StartForm_Logic.cs
@@ -0,0 +1,45 @@
+﻿using QuizSpiel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizDhia2
+{
+    internal class StartForm_Logic
+    {
+        private List<Question> questions;
+        Question question = new Question();
+        User benutzer = new User();
+
+        private void QuestionDecription(TextBox tQuestion)
+        {
+            
+        }
+
+        private void Keyword(TextBox tKeyword)
+        {
+
+        }
+
+
+
+
+        private void CreateQaution()
+        {
+            
+
+        }
+
+        private void Submit(Button bSubmitQaution)
+        {
+
+        }
+
+
+
+
+    }
+}
